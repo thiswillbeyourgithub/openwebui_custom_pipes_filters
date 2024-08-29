8c91767b834d8a9fd786889e01f1b102aa807f6d
thiswillbeyourgithub <26625900+thiswillbeyourgithub@users.noreply.github.com>
2024-08-29 18:09:22 +0200

    first commit

---
+++ pipes/remove_thinking/remove_thinking.py
@@ -0,0 +1,283 @@
+"""
+title: RemoveThinkingPipe
+author: thiswillbeyourgithub
+date: 2024-08-21
+version: 3.1.1
+license: MIT
+description: A pipe function to track user costs and remove 'thinkin' blocks
+"""
+
+from typing import Union, Generator, Iterator, Callable, Any, Optional
+from pydantic import BaseModel, Field
+import requests
+import re
+import json
+
+DEFAULT_BASE_URL = "http://127.0.0.1:4000"
+DEFAULT_CHAT_MODEL = "litellm_sonnet-3.5"
+DEFAULT_TITLE_CHAT_MODEL = "litellm_gpt-4o-mini"
+
+
+class Pipe:
+
+    class Valves(BaseModel):
+        LITELLM_BASE_URL: str = DEFAULT_BASE_URL
+        api_key: Optional[str] = Field(
+            default=None,
+            description="A litellm API api key",
+        )
+        chat_model: str = Field(
+            default=DEFAULT_CHAT_MODEL, description="Chat model to use"
+        )
+        title_chat_model: str = Field(
+            default=DEFAULT_TITLE_CHAT_MODEL,
+            description="Model to use to generate titles",
+        )
+        remove_thoughts: bool = Field(
+            default=True, description="True to remove the thoughts block"
+        )
+        start_thought: str = Field(
+            default="``` ?thinking", description="Start of thought block"
+        )
+        stop_thought: str = Field(default="```", description="End of thought block")
+        debug: bool = Field(
+            default=False,
+            description="Set to True to print more info to the docker logs, also to not remove the last emitter message.",
+        )
+
+    def __init__(self):
+        # You can also set the pipelines that are available in this pipeline.
+        # Set manifold to True if you want to use this pipeline as a manifold.
+        # Manifold pipelines can have multiple pipelines.
+        # self.type = "manifold"
+
+        # Optionally, you can set the id and name of the pipeline.
+        # Best practice is to not specify the id so that it can be automatically inferred from the filename, so that users can install multiple versions of the same pipeline.
+        # The identifier must be unique across all pipelines.
+        # The identifier must be an alphanumeric string that can include underscores or hyphens. It cannot contain spaces, special characters, slashes, or backslashes.
+        self.id = "cost_tracking_pipe"
+
+        # Optionally, you can set the name of the manifold pipeline.
+        self.name = "RemoveThinkingPipe"
+
+        # Initialize rate limits
+        self.valves = self.Valves()
+
+        self.start_thought = re.compile(self.valves.start_thought)
+        self.stop_thought = re.compile(self.valves.stop_thought)
+        self.pattern = re.compile(
+            self.valves.start_thought + "(.*)?" + self.valves.stop_thought,
+            flags=re.DOTALL | re.MULTILINE,
+        )
+
+    async def on_valves_updated(self):
+        """This function is called when the valves are updated."""
+
+        # just checking the validity of the api_key
+        api_key = self.valves.api_key
+        assert isinstance(
+            api_key, str
+        ), f"Expected api_key to be a str, not {type(api_key)}"
+        api_key = api_key.strip()
+        assert api_key, "Api_key is empty"
+
+        self.start_thought = re.compile(self.valves.start_thought)
+        self.stop_thought = re.compile(self.valves.stop_thought)
+        self.pattern = re.compile(
+            self.valves.start_thought + "(.*)?" + self.valves.stop_thought,
+            flags=re.DOTALL | re.MULTILINE,
+        )
+
+
+    async def pipe(
+        self,
+        body: dict,
+        __user__: dict,
+        __event_emitter__: Callable[[dict], Any] = None,
+        # this is just to debug if there are breaking changes etc
+        *args,
+        **kwargs,
+    ) -> Union[str, Generator, Iterator]:
+
+        apikey = self.valves.api_key
+
+        # prints and emitter to show progress
+        def pprint(message: str) -> str:
+            print(f"RemoveThinkingPipe of '{__user__['name']}': " + str(message))
+            return message
+
+        emitter = EventEmitter(__event_emitter__)
+        clear_emitter = not self.valves.debug
+
+        async def prog(message: str) -> None:
+            await emitter.progress_update(pprint(message))
+
+        async def succ(message: str) -> None:
+            await emitter.success_update(pprint(message))
+
+        async def err(message: str) -> None:
+            nonlocal clear_emitter
+            clear_emitter = False
+            await emitter.error_update(pprint(message))
+
+        # to know in the future if there are new arguments I could use
+        if args or kwargs:
+            if args:
+                pprint("Received args:" + str(args))
+            if kwargs:
+                pprint("Received kwargs:" + str(kwargs))
+
+        if self.valves.debug:
+            pprint(body.keys())
+            pprint(body)
+
+        # match the api key
+        await prog("Start")
+        headers = {}
+        headers["Authorization"] = f"Bearer {apikey}"
+
+        try:
+            if body["stream"]:
+                model = self.valves.chat_model
+            else:
+                # stream disabled is only used for the summary title creator AFAIK
+                model = self.valves.title_chat_model
+            payload = body.copy()
+            payload["model"] = model
+
+            await prog("Waiting for response")
+            r = requests.post(
+                url=f"{self.valves.LITELLM_BASE_URL}/v1/chat/completions",
+                json=payload,
+                headers=headers,
+                stream=True,
+            )
+            r.raise_for_status()
+            assert r.status_code == 200, f"Invalid status code: {r.status_code}"
+
+            if body["stream"]:
+                await prog("Receiving chunks")
+
+                # disabled, return all directly
+                if not self.valves.remove_thoughts:
+                    for line in r.iter_lines():
+                        yield line
+                    if clear_emitter:
+                        await succ("")  # hides it
+                    return
+
+                buffer = ""
+                len_start_thought = int(1.5 * len(self.valves.start_thought))
+
+                # remove thoughts
+                thought_removed = 0
+                for line in r.iter_lines():
+                    if not line:
+                        continue
+
+                    # parse content
+                    line = line.decode("utf-8")
+                    if line.startswith("data: "):
+                        line = line[6:]  # Remove "data: " prefix
+                    if line.strip() == "[DONE]":
+                        break
+                    try:
+                        parsed_line = json.loads(line)
+                    except (json.JSONDecodeError, KeyError):
+                        continue
+                    content = parsed_line["choices"][0]["delta"].get("content", "")
+
+                    if not content:
+                        continue
+                    buffer += content
+
+                    match = self.pattern.search(buffer)
+                    if match:  # Remove the thought block
+                        buffer = buffer[: match.start()] + buffer[match.end() :]
+                        while buffer.startswith("\n"):
+                            buffer = buffer[1:]
+                        while buffer.endswith("\n"):
+                            buffer = buffer[:-1]
+                        thought_removed += 1
+                        await succ(f"Removed {thought_removed} thought block")
+
+                    if buffer:
+                        # remove ulterior thought blocks
+                        start_match = self.start_thought.search(buffer)
+                        if start_match:
+                            await prog(f"Waiting for thought n°{thought_removed} to finish")
+
+                        # TODO: actually the start_thought is not of the same length as its pattern but in most cases it's a good upper bound
+                        elif len(buffer) > len_start_thought:
+                            to_yield = buffer[:-len_start_thought]
+                            buffer = buffer[-len_start_thought:]
+                            while buffer.startswith("\n"):
+                                buffer = buffer[1:]
+                            while buffer.endswith("\n"):
+                                buffer = buffer[:-1]
+                            yield to_yield
+
+                if buffer:  # Yield any remaining content with finish_reason "stop"
+                    if self.pattern.search(buffer):
+                        match = self.pattern.search(buffer)
+                        buffer = buffer[: match.start()] + buffer[match.end() :]
+                        while buffer.startswith("\n"):
+                            buffer = buffer[1:]
+                        while buffer.endswith("\n"):
+                            buffer = buffer[:-1]
+                        thought_removed += 1
+                        yield buffer
+                        await succ(f"Removed {thought_removed} thought block")
+
+                    elif self.start_thought.search(buffer):
+                        await err("It seems a thought was never finished")
+                        yield buffer
+                    else:
+                        # await succ(f"Was waiting for a buffer bit: {buffer}")
+                        yield buffer
+
+                if not thought_removed:
+                    # model didn't produce a thought (for example can happen for the chat title)
+                    await err("Thought block never found")
+
+            else:  # return the whole text directly
+                await prog("Returning directly")
+                j = r.json()
+                to_yield = j["choices"][0]["message"].get("content", "")
+                yield to_yield
+
+            if clear_emitter:
+                await succ("")  # hides it
+            return
+
+        except Exception as e:
+            await err(f"Error: {e}")
+            raise
+
+
+class EventEmitter:
+    def __init__(self, event_emitter: Callable[[dict], Any] = None):
+        self.event_emitter = event_emitter
+
+    async def progress_update(self, description):
+        await self.emit(description)
+
+    async def error_update(self, description):
+        await self.emit(description, "error", True)
+
+    async def success_update(self, description):
+        await self.emit(description, "success", True)
+
+    async def emit(self, description="Unknown State", status="in_progress", done=False):
+        if self.event_emitter:
+            await self.event_emitter(
+                {
+                    "type": "status",
+                    "data": {
+                        "status": status,
+                        "description": description,
+                        "done": done,
+                    },
+                }
+            )
+
