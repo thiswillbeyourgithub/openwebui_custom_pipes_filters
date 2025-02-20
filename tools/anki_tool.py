c7352695d95925a0832a6af79cbb58537b3f6bd0
thiswillbeyourgithub <26625900+thiswillbeyourgithub@users.noreply.github.com>
2025-02-20 18:41:15 +0100

    bump version
    Signed-off-by: thiswillbeyourgithub <26625900+thiswillbeyourgithub@users.noreply.github.com>

--- tools/anki_tool.py
+++ tools/anki_tool.py
@@ -5,7 +5,7 @@ author_url: https://github.com/thiswillbeyourgithub
 open_webui_url: https://openwebui.com/t/qqqqqqqqqqqqqqqqqqqq/ankiflashcardcreator/
 git_url: https://github.com/thiswillbeyourgithub/openwebui_custom_pipes_filters
 description: A tool to create Anki flashcards through Ankiconnect with configurable settings and event emitters for UI feedback. Not: if you want a multi user multi anki setup (each user with its own anki) you want each user to add its own private tool with as host a local url to its host via reverse proxies like ngrok that allows a url to point to a local service on the client side.
-version: 1.0.2
+version: 1.0.3
 """
 # Note to dev: don't forget to update the version number inside the Tool class!
 
@@ -71,7 +71,7 @@ Each keys of the param `fields` must be among those fields and all values must b
 
 class Tools:
 
-    VERSION: str =  "1.0.1"
+    VERSION: str =  "1.0.3"
 
     class Valves(BaseModel):
         ankiconnect_host: str = Field(
