4ae3c788f7db4fecd4932c664d9a29e4130085f3
thiswillbeyourgithub <26625900+thiswillbeyourgithub@users.noreply.github.com>
2025-02-22 17:34:44 +0100

    progress on langfuse filter
    Signed-off-by: thiswillbeyourgithub <26625900+thiswillbeyourgithub@users.noreply.github.com>

--- filters/WIP_languse_filter.py
+++ filters/WIP_languse_filter.py
@@ -10,10 +10,11 @@ license: GPLv3
 description: A Filter that prints arguments as they go through it
 openwebui_url: https://openwebui.com/f/qqqqqqqqqqqqqqqqqqqq/langfuse_filter
 ---
-requirements: langfuse>=2.59.3
+requirements: langfuse>=2.59.3,filelock
 ---
 """
 
+from pathlib import Path
 import os
 import time
 import json
@@ -21,30 +22,10 @@ from pydantic import BaseModel, Field
 from typing import Optional, Callable, Any, List
 from langfuse.decorators import observe, langfuse_context
 from datetime import datetime
+from filelock import Timeout, FileLock
 
-def p(message: str) -> None:
-    print(f"LangfuseFilter: {message}")
-
-class Singleton:
-    _instance = None
-    _cnt = None
-    buffer = None
-
-    def __new__(cls):
-        if cls._instance is None:
-            cls._instance = super().__new__(cls)
-        return cls._instance
-
-    def __init__(self):
-        # This will run multiple times if constructor is called multiple times
-        if self._cnt is None:
-            self._cnt = 0
-        if self.buffer is None:
-            self.buffer = {}
-        self._cnt += 1
-        p(f"Singleton count: {self._cnt}")
-
-holder = Singleton()
+LOCK_FILENAME="./langfuse_filter.lock"
+BUFFER=Path("./langfuse_filter.buffer")
 
 class Filter:
     VERSION="1.0.0"
@@ -62,37 +43,14 @@ class Filter:
     def __init__(self):
         self.valves = self.Valves()
         self.debug = self.valves.debug
+        self.lock = FileLock(LOCK_FILENAME,timeout=1)
+
 
     async def log(self, message: str, force: bool = False) -> None:
         if self.valves.debug or force:
             print(f"LangfuseFilter: {message}")
         if force:
             await self.emitter.error_update(f"LangfuseFilter: {message}")
-
-
-    async def inlet(
-        self,
-        body: dict,
-        __user__: Optional[dict] = None,
-        __metadata__: Optional[dict] = None,
-        __model__: Optional[dict] = None,
-        __files__: Optional[list] = None,
-        __event_emitter__: Callable[[dict], Any] = None,
-        ) -> dict:
-        self.emitter = EventEmitter(__event_emitter__)
-        if not "LANGFUSE_HOST" in os.environ:
-            self.log("INLET ERROR: LANGFUSE_HOST not in env", force=True)
-        if not "LANGFUSE_PUBLIC_KEY" in os.environ:
-            self.log("INLET ERROR: LANGFUSE_PUBLIC_KEY not in env", force=True)
-        if not "LANGFUSE_SECRET_KEY" in os.environ:
-            self.log("INLET ERROR: LANGFUSE_SECRET_KEY not in env", force=True)
-        chat_id = __metadata__["chat_id"]
-        if chat_id in holder.buffer:
-            self.log(f"INLET ERROR: holder already contains chat_id '{chat_id}': '{holder.buffer[chat_id]}'", force=True)
-
-        self.log(f"Started timer for chat_id {chat_id}")
-        holder.buffer[chat_id] = datetime.now()
-        return body
 
     def flatten_dict(self, input: dict) -> dict:
         input = input.copy()
@@ -113,6 +71,38 @@ class Filter:
                     input[k2] = v2
         return input
 
+
+    async def inlet(
+        self,
+        body: dict,
+        __event_emitter__: Callable[[dict], Any] = None,
+        ) -> dict:
+        self.emitter = EventEmitter(__event_emitter__)
+        if not "LANGFUSE_HOST" in os.environ:
+            self.log("INLET ERROR: LANGFUSE_HOST not in env", force=True)
+        if not "LANGFUSE_PUBLIC_KEY" in os.environ:
+            self.log("INLET ERROR: LANGFUSE_PUBLIC_KEY not in env", force=True)
+        if not "LANGFUSE_SECRET_KEY" in os.environ:
+            self.log("INLET ERROR: LANGFUSE_SECRET_KEY not in env", force=True)
+        chat_id = __metadata__["chat_id"]
+        with self.lock:
+            try:
+                content = BUFFER.read_text()
+                buffer = json.loads(content)
+                assert isinstance(buffer, dict), f"Not a dict but {type(buffer)}"
+            except Exception as e:
+                self.log(f"LangfuseFilterInlet: Error when loading json: '{e}'\nBuffer: {content}")
+                await self.emitter.error_update(f"LangfuseFilterInlet: Error when loading json: '{e}'\nBuffer: {content}")
+                buffer = {}
+
+            if chat_id in buffer:
+                self.log(f"INLET ERROR: holder already contains chat_id '{chat_id}': '{holder.buffer[chat_id]}'", force=True)
+
+            self.log(f"Started timer for chat_id {chat_id}")
+            buffer[chat_id] = time.time()
+            BUFFER.write_text(json.dumps(buffer))
+        return body
+
     def outlet(
         self,
         body: dict,
@@ -125,11 +115,6 @@ class Filter:
         t_end = datetime.now()
         self.emitter = EventEmitter(__event_emitter__)
         chat_id = __metadata__["chat_id"]
-        if chat_id not in holder.buffer:
-            self.log(f"OUTLET ERROR: holder is missing chat_id '{chat_id}'", force=True)
-            t_start = None
-        else:
-            t_start = holder.buffer[chat_id]
 
         metadata={
             "ow_message_id": __metadata__["message_id"],
@@ -148,42 +133,61 @@ class Filter:
         model_parameters = self.flatten_dict(___model___)
         files = self.flatten_dict(__files__)
 
-        # source: https://langfuse.com/docs/sdk/python/low-level-sdk
+        with self.lock:
+            try:
+                content = BUFFER.read_text()
+                buffer = json.loads(content)
+                assert isinstance(buffer, dict), f"Not a dict but {type(buffer)}"
+            except Exception as e:
+                self.log(f"LangfuseFilterOutlet: Error when loading json: '{e}'\nBuffer: {content}")
+                await self.emitter.error_update(f"LangfuseFilterOutlet: Error when loading json: '{e}'\nBuffer: {content}")
+                buffer = {}
 
-        @observe(as_type="generation")
-        def the_call(messages: List[dict]):
-            langfuse_context.update_current_trace(
-                # name="OpenWebuiLangfuseFilter",
-                name=messages[-1]["content"][:100],
+            if chat_id not in buffer:
+                self.log(f"OUTLET ERROR: holder is missing chat_id '{chat_id}'", force=True)
+                t_start = None
+            else:
+                t_start = datetime.fromtimestamp(float(buffer[chat_id]))
 
-                id= __metadata__["message_id"],
-                session_id=__metadata__["session_id"],
-                chat_id=chat_id,
-                user_id=__user__["name"],
-                model= __model__['info']["base_model_id"],
-                input=messages,
-                output=body["messages"][-1],
+            @observe(as_type="generation")
+            def the_call(messages: List[dict]):
+                """
+                source: https://langfuse.com/docs/sdk/python/low-level-sdk
+                """
+                langfuse_context.update_current_trace(
+                    # name="OpenWebuiLangfuseFilter",
+                    name=messages[-1]["content"][:100],
 
-                model_parameters=model_parameters,
-                metadata=metadata,
+                    id= __metadata__["message_id"],
+                    session_id=__metadata__["session_id"],
+                    chat_id=chat_id,
+                    user_id=__user__["name"],
+                    model= __model__['info']["base_model_id"],
+                    input=messages,
+                    output=body["messages"][-1],
 
-                tags=["open-webui", "langfuse_filter"],
+                    model_parameters=model_parameters,
+                    metadata=metadata,
 
-                public=False,
-                version=self.VERSION,
+                    tags=["open-webui", "langfuse_filter"],
+
+                    public=False,
+                    version=self.VERSION,
 
 
-                start_time=t_start,
-                end_time=t_end,
-            )
-            # return the assistant messsage
-            return body["messages"][-1]
+                    start_time=t_start,
+                    end_time=t_end,
+                )
+                # return the assistant messsage
+                return body["messages"][-1]
 
-        # send every messages except the assisttant answer
-        the_call(body["messages"][:-1])
+            # send every messages except the assisstant answer
+            the_call(body["messages"][:-1])
 
-        if chat_id in holder.buffer and holder.buffer[chat_id] == t_start:
-            del holder.buffer[chat_id]
+            if chat_id in buffer and buffer[chat_id] == t_start:
+                del buffer[chat_id]
+
+            BUFFER.write_text(json.dumps(buffer))
 
         self.log(f"Done with langfuse with chat_id {chat_id}")
         return body
