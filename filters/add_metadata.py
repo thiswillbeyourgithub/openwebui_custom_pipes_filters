05c8fcd19c2fc0aa259b27efb0662cf3e109b5bd
thiswillbeyourgithub <26625900+thiswillbeyourgithub@users.noreply.github.com>
2025-02-24 23:32:46 +0100

    bump version
    Signed-off-by: thiswillbeyourgithub
    <26625900+thiswillbeyourgithub@users.noreply.github.com>

--- filters/add_metadata.py
+++ filters/add_metadata.py
@@ -4,7 +4,7 @@ author: thiswillbeyourightub
 author_url: https://github.com/thiswillbeyourgithub/openwebui_custom_pipes_filters/
 funding_url: https://github.com/thiswillbeyourgithub/openwebui_custom_pipes_filters/
 git_url: https://github.com/thiswillbeyourgithub/openwebui_custom_pipes_filters
-version: 1.1.0
+version: 1.1.1
 date: 2024-08-29
 license: GPLv3
 description: A Filter that adds user and other type of metadata to the requests. Made for litellm set to use langfuse callbacks.
@@ -27,7 +27,7 @@ def load_json_dict(user_value: str) -> dict:
 
 
 class Filter:
-    VERSION: str = "1.1.0"
+    VERSION: str = "1.1.1"
     class Valves(BaseModel):
         priority: int = Field(
             default=0,
