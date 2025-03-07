2473de6864c4bdaafcaf71f7e3c35f82a43df756
thiswillbeyourgithub <26625900+thiswillbeyourgithub@users.noreply.github.com>
2025-03-07 22:11:53 +0100

    fix: typo
    Signed-off-by: thiswillbeyourgithub <26625900+thiswillbeyourgithub@users.noreply.github.com>

--- tools/wdoc_tools.py
+++ tools/wdoc_tools.py
@@ -5,7 +5,7 @@ author_url: https://github.com/thiswillbeyourgithub/openwebui_custom_pipes_filte
 funding_url: https://github.com/thiswillbeyourgithub/openwebui_custom_pipes_filters/
 git_url: https://github.com/thiswillbeyourgithub/openwebui_custom_pipes_filters/
 description: Use wdoc to parse urls and files
-runding_url: https://github.com/open-webui
+funding_url: https://github.com/open-webui
 version: 1.0.0
 license: GPLv3
 # requirements: wdoc>=2.6.7  # commented to instead install it in the tool itself and avoid uninstalling open-webui dependencies
