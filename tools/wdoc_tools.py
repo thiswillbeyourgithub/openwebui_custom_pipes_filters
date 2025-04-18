c635f146b92bdd2c35c684504e26230425b4de91
thiswillbeyourgithub <26625900+thiswillbeyourgithub@users.noreply.github.com>
2025-04-18 15:20:37 +0200

    bump versionto 3.1.0
    Signed-off-by: thiswillbeyourgithub <26625900+thiswillbeyourgithub@users.noreply.github.com>

--- tools/wdoc_tools.py
+++ tools/wdoc_tools.py
@@ -70,7 +70,7 @@ class Tools:
     VERSION: str = [li for li in __doc__.splitlines() if li.startswith("version: ")][
         0
     ].split("version: ")[1]
-    APPROPRIATE_WDOC_VERSION: str = "3.0.2"
+    APPROPRIATE_WDOC_VERSION: str = "3.1.0"
 
     class Valves(BaseModel):
         useracknowledgement: bool = Field(
