b6770412c018d0ea5e7a33b00e32ff573fb825b7
thiswillbeyourgithub <26625900+thiswillbeyourgithub@users.noreply.github.com>
2025-04-18 11:35:39 +0200

    add description
    Signed-off-by: thiswillbeyourgithub <26625900+thiswillbeyourgithub@users.noreply.github.com>

--- filters/debug_filter.py
+++ filters/debug_filter.py
@@ -7,7 +7,7 @@ git_url: https://github.com/thiswillbeyourgithub/openwebui_custom_pipes_filters
 version: 2.3.0
 date: 2025-03-23
 license: GPLv3
-description: Filter that prints argument as they pass through it. You can use it multiple times to debug another filter.
+description: Filter that prints argument as they pass through it. You can use it multiple times to debug another filter. You can then use `docker logs open-webui --tail 100 --follow | grep debugfilter` to see the data that interests you.
 openwebui_url: https://openwebui.com/f/qqqqqqqqqqqqqqqqqqqq/debug_filter
 """
 
